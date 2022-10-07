e9aa9c32855aa47a05c398e66cc016b7d54ccfde
JC Guerrero <JCystems@gMail.com>
2022-10-07 15:01:43 -0500

    feat(brew install az-cli)
    Added it to package.json

---
+++ scripts/azure/cli/install.ps1
@@ -0,0 +1,6 @@
+
+# SRC: https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=powershell
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+rm .\AzureCLI.msi
