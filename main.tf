792549014fa6192868cb92f1bbd5060e87ce1c51
Juan Guerrero <JCystems@gMail.com>
2022-11-14 11:04:36 -0600

    revert(azure)

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-module "azure" {
-  source = "./terra/azure/centralus"
-}
+# FIXME
+# module "azure" {
+#   source = "./terra/azure/centralus"
+# }
 
 module "hello_world" {
   source = "./terra/hello_world"
